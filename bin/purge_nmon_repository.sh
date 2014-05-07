4b11137970042c3a9827680616e9116f7c4fe76e
Glitch Cowboy <splunk@glitch-cowboy.com>
2014-05-07 10:25:09 -0500

    Neutralized OS-specificity

--- bin/purge_nmon_repository.sh
+++ bin/purge_nmon_repository.sh
@@ -12,17 +12,25 @@
 ## 	Your Customizations Go Here            ##
 #################################################
 
+if [ $(echo $SPLUNK_HOME|grep -q forwarder) ];then
+	APP=$SPLUNK_HOME/etc/apps/TA-nmon
+elif [ -d $SPLUNK_HOME/etc/slave-apps/_cluster ];then 
+	APP=$SPLUNK_HOME/etc/slave-apps/PA-nmon
+else
+	APP=$SPLUNK_HOME/etc/apps/nmon
+fi
+
 
 # Splunk Home variable: This should automatically defined when this script is being launched by Splunk
 # If you intend to run this script out of Splunk, please set your custom value here
 SPL_HOME=${SPLUNK_HOME}
 
 # Clean various dir
-NMON_REPO=${SPL_HOME}/etc/apps/nmon/var/nmon_repository
-CONFIG_REPO=${SPL_HOME}/etc/apps/nmon/var/config_repository
-CSV_REPO=${SPL_HOME}/etc/apps/nmon/var/csv_repository
-SPOOL=${SPL_HOME}/etc/apps/nmon/var/spool
-TEMP=${SPL_HOME}/etc/apps/nmon/var/nmon_temp
+NMON_REPO=${APP}/var/nmon_repository
+CONFIG_REPO=${APP}/var/config_repository
+CSV_REPO=${APP}/var/csv_repository
+SPOOL=${APP}/var/spool
+TEMP=${APP}/var/nmon_temp
 
 
 # mtime days used for purge, every nmon file older than X days will be purged
